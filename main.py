795cba3035910cb0c25f38e1ba1af70603087535
Sayko741 <mohamedeslammaklad700@gmail.com>
2026-05-30 04:52:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+import yt_dlp
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    msg = await update.message.reply_text("⏳ جاري التحميل...")
+
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
+        'noplaylist': True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            file_path = ydl.prepare_filename(info)
+
+        if os.path.exists(file_path):
+            with open(file_path, "rb") as video_file:
+                await update.message.reply_video(video=video_file)
+
+            os.remove(file_path)
+        else:
+            await update.message.reply_text("❌ الملف مش موجود بعد التحميل")
+
+    except Exception as e:
+        await update.message.reply_text(f"❌ حصل خطأ:\n{e}")
+
+    await msg.delete()
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
+
+print("Bot Running...")
+app.run_polling()
